f1667645d96104ae668f02ff40ff5cf635a21a0d
Ze Yu Zhong <zeyu123@gmail.com>
2020-01-15 12:14:55 +1100

    sagemaker prediction code

--- R/AWS/sagemaker.R
+++ R/AWS/sagemaker.R
@@ -7,6 +7,7 @@
 # Set up an EC2 instance, doesn't matter too much what's included
 # At least t2.medium is recommend due to RAM constraints on the free tier (this is still fairly cheap)
 # Note that as most of the actual computation is done via sagemaker, EC2 costs should be quite low
+# Sagemaker fees are quite expensive though unfortunately
 # I preferred an Ubuntu image because there's a bit more documentation
 # 
 # Create the following IAM roles:
@@ -96,6 +97,8 @@ s3_output_path <- paste0("s3://", s3_bucket, "/output/")
 
 session$download_data("data", s3_bucket, key_prefix = "data/pooled_panel.rds")
 
+session$download_data("data", s3_bucket, key_prefix = "data/pooled_panel.json")
+
 pooled_panel <- readRDS("data/pooled_panel.rds")
 
 ###
@@ -115,6 +118,60 @@ pooled_panel <- readRDS("data/pooled_panel.rds")
 ## DeepAR can also support missing data in the target series (y variable), but not the dyanmic features
 ## This makes it a little strange to work with for our dataset
 
+###########################################################################
+library(jsonlite)
+###########################################################################
+## Function that takes data frame in tidy format, and outputs a dataframe 
+## that is ready to be exported to JSON for use with sagemaker deepar
+
+tidy_to_json <- function(data, start) {
+  stock_id <- data$stock %>%
+    unique()
+  
+  # Number of cross sectional units
+  cross_units <- length(stock_id)
+  
+  ## JUst setting the beginning time to something arbitrary for now, change if needed
+  pooled_panel_json <- data.frame(start = rep(start, cross_units), 
+                                  target = c(1:cross_units), 
+                                  dynamic_feat = c(1:cross_units))
+  
+  for (i in 1:cross_units) {
+    pooled_panel_filter <- data %>%
+      filter(stock == stock_id[i])
+    
+    pooled_panel_json$target[i] <- list(pooled_panel_filter$rt)
+    
+    pooled_panel_filter_feature <- pooled_panel_filter %>%
+      select(-time, -rt, -stock) %>%
+      unname() %>%
+      as.matrix() %>%
+      # Transpose it to get the right format of one feature series per row
+      t()
+    
+    pooled_panel_json[i, ]$dynamic_feat <- pooled_panel_filter_feature %>% list()
+  }
+  pooled_panel_json
+}
+
+pooled_panel_train <- pooled_panel %>%
+  filter(time <= 168) %>%
+  tidy_to_json(start = "2000-01-01 00:00:00")
+
+pooled_panel_test <- pooled_panel %>%
+  filter(time > 168) %>%
+  tidy_to_json(start = "2014-01-01 00:00:00")
+
+pooled_panel_train %>%
+  stream_out(file("./data/pooled_panel_train.json"))
+
+pooled_panel_test %>%
+  stream_out(file("./data/pooled_panel_test.json"))
+
+## Upload to S3, note that this function uploads the entire directory you specify
+
+session$upload_data("data", s3_bucket, key_prefix = "data")
+
 #######################################
 ## Get the image required for deepar ##
 #######################################
@@ -133,45 +190,152 @@ estimator <- sagemaker$estimator$Estimator(
   sagemaker_session = session,
   image_name = image_name,
   role = role_arn,
-  train_instance_count = 1,
-  train_instance_type = 'ml.c4.2xlarge',
+  train_instance_count = 1L,
+  train_instance_type = 'ml.c4.xlarge',
   base_job_name = 'simulation_deepar',
   output_path = s3_output_path
 )
+
+# Lags are used in the model building procedure anyway, so context_length doesn't have to be very large
+# Amazon recommends to just set this equal to prediction length
+context_length <- 12L
+prediction_length <- 12L
+freq <- "1M"
   
-estimator$set_hyperparameters(num_round = 100L)
+estimator$set_hyperparameters(
+  time_freq = "1M",
+  context_length = context_length,
+  prediction_length = prediction_length,
+  num_cells = 40L,
+  num_layers = 4L,
+  likelihood = "student-T",
+  epochs = 80L,
+  mini_batch_size = 128L,
+  learning_rate = "0.0001",
+  dropout_rate = 0.15,
+  early_stopping_patience = 20L,
+  num_dynamic_feat = "auto",
+  test_quantiles = array(0.5)
+)
+
+s3_train_input <- "s3://sagemaker-us-west-2-438078873022/data/pooled_panel_train.json"
+s3_valid_input <- "s3://sagemaker-us-west-2-438078873022/data/pooled_panel_test.json"
+
+data_channels <- list('train' = s3_train_input, 'test' = s3_valid_input)
+
 job_name <- paste('sagemaker-simulation-deepar', format(Sys.time(), '%H-%M-%S'), sep = '-')
-input_data <- list('train' = s3_train_input,
-                   'validation' = s3_valid_input)
-estimator$fit(inputs = input_data,
+estimator$fit(inputs = data_channels,
               job_name = job_name)
 
-# S3 Path
+############################################################################
+## HYPERPARAMETER TUNING
+############################################################################
 
-estimator$model_data
+## NOT RUN, as it seems fairly robust to hyperparameters (ie they don't help much), 
+## and very costly and time consuming
 
-## Generating Predictions
+## Import some functions from
+
+# HyperparameterTuner <- sagemaker$tuner$HyperparameterTuner
+# IntegerParameter <- sagemaker$tuner$IntegerParameter
+# CategoricalParameter <- sagemaker$tuner$CategoricalParameter
+# ContinuousParameter <- sagemaker$tuner$ContinuousParameter
+# 
+# hyperparameter_ranges <- list(
+#   learning_rate = ContinuousParameter(0.0001, 1),
+#   num_cells = IntegerParameter(10L, 100L),
+#   num_layers = IntegerParameter(1L, 4L),
+#   dropout_rate = ContinuousParameter(0, 0.2)
+# )
+# 
+# objective_metric_name <- "test:mean_wQuantileLoss"
+# 
+# tuner = HyperparameterTuner(estimator,
+#                             objective_metric_name,
+#                             objective_type = 'Minimize',
+#                             hyperparameter_ranges,
+#                             max_jobs = 96L,
+#                             max_parallel_jobs = 3L)
+# 
+# tuner$fit(list(train = s3_train_input, test = s3_valid_input), include_cls_metadata = FALSE)
+
+##############################################################################################################
+##############################################################################################################
+#### Generating Predictions
+##############################################################################################################
+##############################################################################################################
+
+###########################
+## MODEL ENDPOINT APPROACH
+##############################
+## An endpoint is like a server which you can send requests to and receive predictions in real time
+## Obviously, designed with real use business cases in mind
+## Downside - quite unintuitive for "normal" usage, and may not work with large number of time series
+## Implemented anyway, as this is the recommended approach
+
+################
+## DATA PREP
+################
+## Function that takes a tidy dataframe, and produce a dataframe in the right format
+## to be converted to the inference JSON format to make predictions
+## Require the tidy_to_json function from before
+## Remember that this returns the DATAFRAME, and to get the actual text format you need to pass to through toJSON
+
+## Note that DeepAR takes entire time series and produces predictions 
+## according to the prediction length argument you specified when building the model
+## Therefore, provide it with the training + validation set data (assuming this isn't too computationally intensive)
+
+json_to_inference_json <- function(json_df) {
+  inf_json <- list(
+    instances = list(start = pooled_panel_json$start, 
+                     target = pooled_panel_json$target, 
+                     dynamic_feat = pooled_panel_json$dynamic_feat),
+    configuration = list(
+      num_samples = 10, output_types = c("mean", "quantiles", "samples"), quantiles = c("0.5")
+    )
+  )
+}
+
+tidy_to_inf_json <- function(dataframe, start) {
+  dataframe_json <- dataframe %>% tidy_to_json(start = start)
+  
+  dataframe_json %>% json_to_inference_json()
+}
+
+####################################
+## Set up model endpoint object
+####################################
 
 model_endpoint <- estimator$deploy(initial_instance_count = 1L,
-                                   instance_type = 'ml.t2.medium')
+                                   instance_type = 'ml.t2.medium',
+                                   content_type="application/json")
 
-model_endpoint$content_type <- 'text/csv'
-model_endpoint$serializer <- sagemaker$predictor$csv_serializer
+## Wrapper function that takes a tidy_df and 
 
-abalone_test <- abalone_test[-1]
-num_predict_rows <- 500
-test_sample <- as.matrix(abalone_test[1:num_predict_rows, ])
-dimnames(test_sample)[[2]] <- NULL
+deepar_predict <- function(tidy_df) {
+  model_endpoint$predict(tidy_df %>% tidy_to_inf_json(start = "2000-01-01 00:00:00"))
+}
 
-library(stringr)
-predictions <- model_endpoint$predict(test_sample)
-predictions <- str_split(predictions, pattern = ',', simplify = TRUE)
-predictions <- as.numeric(predictions)
-
-abalone_test <- cbind(predicted_rings = predictions, 
-                      abalone_test[1:num_predict_rows, ])
-head(abalone_test)  
-
-## Delete endpoint to minimize endpoint costs
+##################################################
+## Delete the endpoint afterwards to save $$$
 
 session$delete_endpoint(model_endpoint$endpoint)
+
+######################################################
+## BATCH TRANSFORM APPROACH
+######################################################
+## Much more analagous to "normal" functions, this "transforms" input data to output data using a trained sagemaker model
+## Also seems to be much more straightforward and well suited to large number of time series
+
+## Specify inference input and output path
+
+batch_input <- paste0("s3://", s3_bucket, "/batch/input/pooled_panel_inference_in.json")
+batch_output <- paste0("s3://", s3_bucket, "/batch/output/pooled_panel_inference_out.json")
+
+transformer = model.transformer(instance_count=1, instance_type='ml.m4.xlarge', output_path = batch_output)
+
+transformer.transform(data=batch_input, data_type='S3Prefix', content_type='text/csv', split_type='Line')
+
+transformer.wait()
+
+
