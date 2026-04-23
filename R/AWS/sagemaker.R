3b0ffa93322eb18ec2de76473237644589648271
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-04-23 23:01:48 +0000

    docs: add roxygen2 documentation for customTimeSlices

--- R/AWS/sagemaker.R
+++ R/AWS/sagemaker.R
@@ -106,6 +106,20 @@ s3_bucket <- session$default_bucket()
 s3_data_path <- paste0("s3://", s3_bucket, "/data/")
 s3_output_path <- paste0("s3://", s3_bucket, "/output/")
 
+# Section Setup ----------------------------------------------------------------
+
+#' Create Custom Time Slices
+#'
+#' Generates a list of time slices for cross-validation testing.
+#' Each slice contains train, validation, and test indices.
+#'
+#' @param start Integer specifying the starting index.
+#' @param initialWindow Integer specifying the size of the initial window.
+#' @param horizon Integer specifying the horizon for each step.
+#' @param validation_size Integer specifying the size of the validation set.
+#' @param test_size Integer specifying the size of the test set.
+#' @param set_no Integer specifying the number of slices to generate.
+#' @return A list of length \code{set_no} containing indices.
 customTimeSlices <- function(start, initialWindow, horizon, validation_size, test_size, set_no) {
   
   time_slice <- list(train = 0, validation = 0, test = 0)
