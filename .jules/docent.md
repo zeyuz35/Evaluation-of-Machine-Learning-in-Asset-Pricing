d3ff17fca947aaafea2653f0bd0a7ee22d7a8808
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-06-12 01:10:36 +0000

    📝 Docent: replace cat with message and remove informal language in xgboost demo

---
+++ .jules/docent.md
@@ -0,0 +1,3 @@
+## 2024-05-24 - Replace raw output and informal language
+**Learning:** Found raw `cat()` used for informational messages and informal language (emoji, contractions) in R/xgboost/demo scripts.
+**Action:** Replaced `cat()` with `message()` to ensure structured logging, and removed emoji/contractions to maintain a professional tone.
--- R/xgboost/demo/create_sparse_matrix.R
+++ R/xgboost/demo/create_sparse_matrix.R
@@ -21,15 +21,15 @@ data(Arthritis)
 # create a copy of the dataset with data.table package (data.table is 100% compliant with R dataframe but its syntax is a lot more consistent and its performance are really good).
 df <- data.table(Arthritis, keep.rownames = F)
 
-# Let's have a look to the data.table
-cat("Print the dataset\n")
+# Let us have a look to the data.table
+message("Print the dataset")
 print(df)
 
 # 2 columns have factor type, one has ordinal type (ordinal variable is a categorical variable with values wich can be ordered, here: None > Some > Marked).
-cat("Structure of the dataset\n")
+message("Structure of the dataset")
 str(df)
 
-# Let's add some new categorical features to see if it helps. Of course these feature are highly correlated to the Age feature. Usually it's not a good thing in ML, but Tree algorithms (including boosted trees) are able to select the best features, even in case of highly correlated features.
+# Let us add some new categorical features to see if it helps. Of course these feature are highly correlated to the Age feature. Usually it is not a good thing in ML, but Tree algorithms (including boosted trees) are able to select the best features, even in case of highly correlated features.
 
 # For the first feature we create groups of age by rounding the real age. Note that we transform it to factor (categorical data) so the algorithm treat them as independant values.
 df[,AgeDiscret:= as.factor(round(Age/10,0))]
@@ -41,20 +41,20 @@ df[,AgeCat:= as.factor(ifelse(Age > 30, "Old", "Young"))]
 df[,ID:=NULL]
 
 # List the different values for the column Treatment: Placebo, Treated.
-cat("Values of the categorical feature Treatment\n")
+message("Values of the categorical feature Treatment")
 print(levels(df[,Treatment]))
 
 # Next step, we will transform the categorical data to dummy variables.
 # This method is also called one hot encoding.
 # The purpose is to transform each value of each categorical feature in one binary feature.
 #
-# Let's take, the column Treatment will be replaced by two columns, Placebo, and Treated. Each of them will be binary. For example an observation which had the value Placebo in column Treatment before the transformation will have, after the transformation, the value 1 in the new column Placebo and the value 0 in the new column  Treated.
+# Let us take, the column Treatment will be replaced by two columns, Placebo, and Treated. Each of them will be binary. For example an observation which had the value Placebo in column Treatment before the transformation will have, after the transformation, the value 1 in the new column Placebo and the value 0 in the new column  Treated.
 #
 # Formulae Improved~.-1 used below means transform all categorical features but column Improved to binary values.
 # Column Improved is excluded because it will be our output column, the one we want to predict.
 sparse_matrix = sparse.model.matrix(Improved~.-1, data = df)
 
-cat("Encoding of the sparse Matrix\n")
+message("Encoding of the sparse Matrix")
 print(sparse_matrix)
 
 # Create the output vector (not sparse)
@@ -64,7 +64,7 @@ print(sparse_matrix)
 output_vector = df[,Y:=0][Improved == "Marked",Y:=1][,Y]
 
 # Following is the same process as other demo
-cat("Learning...\n")
+message("Learning...")
 bst <- xgboost(data = sparse_matrix, label = output_vector, max_depth = 9,
                eta = 1, nthread = 2, nrounds = 10, objective = "binary:logistic")
 
@@ -73,7 +73,7 @@ print(importance)
 # According to the matrix below, the most important feature in this dataset to predict if the treatment will work is the Age. The second most important feature is having received a placebo or not. The sex is third. Then we see our generated features (AgeDiscret). We can see that their contribution is very low (Gain column).
 
 # Does these result make sense?
-# Let's check some Chi2 between each of these features and the outcome.
+# Let us check some Chi2 between each of these features and the outcome.
 
 print(chisq.test(df$Age, df$Y))
 # Pearson correlation between Age and illness disappearing is 35
@@ -82,8 +82,8 @@ print(chisq.test(df$AgeDiscret, df$Y))
 # Our first simplification of Age gives a Pearson correlation of 8.
 
 print(chisq.test(df$AgeCat, df$Y))
-# The perfectly random split I did between young and old at 30 years old have a low correlation of 2. It's a result we may expect as may be in my mind > 30 years is being old (I am 32 and starting feeling old, this may explain that), but  for the illness we are studying, the age to be vulnerable is not the same. Don't let your "gut" lower the quality of your model. In "data science", there is science :-)
+# The perfectly random split I did between young and old at 30 years old have a low correlation of 2. It is a result we may expect as may be in my mind > 30 years is being old (I am 32 and starting feeling old, this may explain that), but  for the illness we are studying, the age to be vulnerable is not the same. Do not let your "gut" lower the quality of your model. In "data science", there is science
 
-# As you can see, in general destroying information by simplifying it won't improve your model. Chi2 just demonstrates that. But in more complex cases, creating a new feature based on existing one which makes link with the outcome more obvious may help the algorithm and improve the model. The case studied here is not enough complex to show that. Check Kaggle forum for some challenging datasets.
-# However it's almost always worse when you add some arbitrary rules.
+# As you can see, in general destroying information by simplifying it will not improve your model. Chi2 just demonstrates that. But in more complex cases, creating a new feature based on existing one which makes link with the outcome more obvious may help the algorithm and improve the model. The case studied here is not enough complex to show that. Check Kaggle forum for some challenging datasets.
+# However it is almost always worse when you add some arbitrary rules.
 # Moreover, you can notice that even if we have added some not useful new features highly correlated with other features, the boosting tree algorithm have been able to choose the best one, which in this case is the Age. Linear model may not be that strong in these scenario.
